005287e10ffd2363a683911fb357125a0ede399f
patto1234 <spatterson6187@gmail.com>
2024-10-16 14:34:40 +1100

    updated

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,8 +6,8 @@ public class CameraController : MonoBehaviour
 {
     public Transform cameraTransform;
 
-    public Vector2 minX, maxX;
-    public Vector2 minY, maxY;
+    public Vector2 minXandY;
+    public Vector2 maxXandY;
 
     
 
@@ -46,14 +46,21 @@ public class CameraController : MonoBehaviour
     }
     void HandleMovementInput()
     {
-        
+        float forwardInput = Input.GetAxis("Vertical");
+        float sideInput = Input.GetAxis("Horizontal");
+
+
+
+        newPosition += sideInput * Vector3.forward + -forwardInput * Vector3.right;
+
+        /*
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            newPosition += (transform.up * movementSpeed);
+            newPosition += (transform.forward * movementSpeed);
         }
         if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            newPosition += (transform.up * -movementSpeed);
+            newPosition += (transform.forward * -movementSpeed);
         }
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
@@ -62,10 +69,13 @@ public class CameraController : MonoBehaviour
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             newPosition += (transform.right * -movementSpeed);
-        }
+        }*/
 
-        newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
-        newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
+
+
+        newPosition.y = 35.4f;
+        newPosition.x = Mathf.Clamp(newPosition.x, minXandY.x, maxXandY.x);
+        newPosition.z = Mathf.Clamp(newPosition.z, minXandY.y, maxXandY.y);
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
         cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
